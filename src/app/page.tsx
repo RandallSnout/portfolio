f61a3f1cd05b283dc816037eb3df59984b0c7a75
RandallSnout <pfeiffer.justin.pfeiffer@gmail.com>
2025-04-25 18:15:11 -0600

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <div className="flex flex-row">
+        <div className="basis-1/2">
+          <h1 className="text-5xl mb-4">My name is Justin and I'm a Product Designer</h1>
+          <h3 className="text-lg">I am a multi faceted Designer with a desire to improve user experience and elicit positive emotional responses. With over a decade of experience creatively solving problems and producing user centered products.</h3>
+        </div>
+        <div className="basis-1/2">
+          <img src="" alt="" className="" />
+        </div>
+      </div>
     </main>
   );
 }
